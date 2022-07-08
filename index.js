800b5295f0f8ba8ff3b81b7f6db956a4baa8c732
bluuweb <contacto@bluuweb.cl>
2022-07-08 15:44:33 -0400

    🍉🍉🍉

--- index.js
+++ index.js
@@ -40,4 +40,4 @@ app.use("/api/v1/links", linkRouter);
 // app.use(express.static("public"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("🔥🔥🔥 http://localhost:" + PORT));
+app.listen(PORT, () => console.log("🍉🍉🍉 http://localhost:" + PORT));
